44950da13f7b3a6453211669e869fc5bd4d3715c
Your NameHashimJafarov <hashim.cfr@gmail.com>
2023-09-27 16:54:34 +0400

    main logic in js is done

--- app.js
+++ app.js
@@ -1,46 +1,34 @@
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
-const swiper = document.querySelector(".swiper");
 const swiperSlides = document.querySelectorAll(".swiper_slide");
-const slideWidth = swiperSlides[0].offsetWidth;
+const pagination = document.querySelector(".pagination");
 let active = 0;
-const moveSlider = (active) => {
-  console.log(active);
-  const position = -active * 880;
-  console.log(position);
-  swiperSlides[active].style.transform = `translateX(${position}px)`;
+
+const createPagination = () => {
+  [...swiperSlides].map((slider, index) => {
+    const page = index + 1;
+    pagination.innerHTML += `<li onclick=getPage(${index})>${page}</li>`;
+  });
+};
+createPagination();
+const getPage = (page) => {
+  swiperSlides[active].classList.remove("show");
+  active = page;
+  swiperSlides[active].classList.add("show");
 };
 nextBtn.addEventListener("click", () => {
+  swiperSlides[active].classList.remove("show");
   active++;
-  if (active >= swiperSlides.length) {
+  if (active > swiperSlides.length - 1) {
     active = 0;
   }
-  moveSlider(active);
-
-  //   swiperSlide[active].style.transform = "translateX(-105.5%)";
-  //   swiperSlide[active].classList.remove("show");
-  //   active++;
-  //   if (active > swiperSlide.length - 1) {
-  //     active = 0;
-  //   }
-
-  //   swiperSlide[active].style.transform = "translateX(-105.5%)";
-  //   swiperSlide[active].classList.add("show");
+  swiperSlides[active].classList.add("show");
 });
 prevBtn.addEventListener("click", () => {
-  if (active > 0) {
-    active--;
-    moveSlider(active);
-  } else {
-    // Wrap around to the last slide when at the first slide
+  swiperSlides[active].classList.remove("show");
+  active--;
+  if (active < 0) {
     active = swiperSlides.length - 1;
-    moveSlider(active);
   }
-
-  //   swiperSlide[active].classList.remove("show");
-  //   active--;
-  //   if (active < 0) {
-  //     active = swiperSlide.length - 1;
-  //   }
-  //   swiperSlide[active].classList.add("show");
+  swiperSlides[active].classList.add("show");
 });
