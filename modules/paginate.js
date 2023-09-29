f2680717d161cd173fc12b6be6b2c89e912d9e8c
Your NameHashimJafarov <hashim.cfr@gmail.com>
2023-09-29 16:12:02 +0400

    commit

--- modules/paginate.js
+++ modules/paginate.js
@@ -1,7 +1,6 @@
 const createPagination = (swiperSlides, pagination, getPage) => {
   [...swiperSlides].map((slider, index) => {
     const page = index + 1;
-    // pagination.innerHTML += `<li onclick=getPage(${index})>${page}</li>`;
     const liElement = document.createElement("li");
     if (index === 0) liElement.classList.add("active");
     liElement.textContent = page;
